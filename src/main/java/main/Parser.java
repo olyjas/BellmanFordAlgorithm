1538b1bd646fdb0351d5c05b3be73f3491c83a29
Jasmine <jaszhang@cs.washington.edu>
2024-02-26 14:16:53 -0800

    ckp 1

--- src/main/java/main/Parser.java
+++ src/main/java/main/Parser.java
@@ -4,6 +4,9 @@ import cse332.exceptions.NotYetImplementedException;
 
 import java.util.List;
 import java.util.Map;
+import cse332.graph.GraphUtil;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Parser {
 
@@ -13,7 +16,22 @@ public class Parser {
      * @return Adjacency list of maps from node to weight
      */
     public static List<Map<Integer, Integer>> parse(int[][] adjMatrix) {
-        throw new NotYetImplementedException();
+       int vertNum = adjMatrix.length;
+       List<Map<Integer, Integer>> adjList = new ArrayList<>(vertNum);
+
+       // initializing adjacency list with empty maps for each vertex
+       for (int i = 0; i < vertNum; i++) {
+           adjList.add(new HashMap<>());
+       }
+        // adding neighbors to adjacency list
+        for (int i = 0; i < vertNum; i++) {
+            for (int j = 0; j < vertNum; j++) {
+                if (adjMatrix[i][j] != Integer.MAX_VALUE) {
+                    adjList.get(i).put(j, adjMatrix[i][j]);
+                }
+            }
+        }
+        return adjList;
     }
 
     /**
--- src/main/java/solvers/OutSequential.java
+++ src/main/java/solvers/OutSequential.java
@@ -3,6 +3,8 @@ package solvers;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.BellmanFordSolver;
 import main.Parser;
+import cse332.graph.GraphUtil;
+import main.Parser;
 
 import java.util.List;
 import java.util.Map;
@@ -10,9 +12,43 @@ import java.util.Map;
 public class OutSequential implements BellmanFordSolver {
 
     public List<Integer> solve(int[][] adjMatrix, int source) {
+        // maps = outgoing edges from vertex
+        // keys = destination vertices
+        // values = weights of corresponding edges
         List<Map<Integer, Integer>> g = Parser.parse(adjMatrix);
+        // initializing variables for number of vertices, distances, predecessors
+        int vertNum = adjMatrix.length;
+        int[] dist = new int[vertNum];
+        int[] pred = new int[vertNum];
 
-        throw new NotYetImplementedException();
+        for (int i = 0; i < vertNum; i++) {
+            dist[i] = GraphUtil.INF;
+            pred[i] = -1;
+        }
+        dist[source] = 0;
+
+        // Bellman-ford algorithm iterations
+        for (int i = 0; i < vertNum; i++) {
+            int[] copiedDist = new int[vertNum];
+
+            // copy of current distances for relaxation
+            for (int j = 0; j < vertNum; j++) {
+                copiedDist[j] = dist[j];
+            }
+
+            // relaxing edges in graph
+            for (int j = 0; j < vertNum; j++) {
+                Map<Integer, Integer> neighbors = g.get(j);
+                for (Integer edge : neighbors.keySet()) {
+                    // checks if distance can be updated based on relaxation
+                    if (copiedDist[j] != GraphUtil.INF && neighbors.get(edge) != GraphUtil.INF && copiedDist[j] + neighbors.get(edge) < dist[edge]) {
+                        dist[edge] = copiedDist[j] + neighbors.get(edge);
+                        pred[edge] = j;
+                    }
+                }
+            }
+        }
+        // return list of vertices involved in negative cycle
+        return GraphUtil.getCycle(pred);
     }
-
 }
